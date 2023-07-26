54a4a9a412a7fff134c4adcad44ea71086d8b6a0
eugeniord <58301986+eugeniord@users.noreply.github.com>
2023-07-27 00:59:25 +0100

    Time: 7 ms (89.74%) | Memory: 8.1 MB (89.74%) - LeetSync

--- 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.cpp
+++ 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.cpp
@@ -1,24 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-    }
-
-    Node(int _val, Node* _next) {
-        val = _val;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* insert(Node* head, int insertVal) {
@@ -27,11 +6,7 @@ public:
             newNode->next = newNode;
             return newNode;
         }
-        if (head->next == head) {
-            Node* newNode = new Node(insertVal, head);
-            head->next = newNode;
-            return head;
-        }
+        
         Node* prev = head;
         Node* curr = head->next;
         bool insert = false;
@@ -39,10 +14,12 @@ public:
         do{
             if (prev->val <= insertVal && insertVal <= curr->val) {
                 break;
-            } else if (prev->val > curr->val &&
-                (insertVal >= prev->val || insertVal <= curr->val)) {
+            } 
+            
+            if (prev->val > curr->val && (insertVal >= prev->val || insertVal <= curr->val)) {
                 break;
             }
+
             prev = curr;
             curr = curr->next;
         } while (prev != head);
