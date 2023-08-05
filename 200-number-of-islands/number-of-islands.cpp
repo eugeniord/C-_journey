924e4a1e0d6bd6b0a747571f08aebc24c21f6c78
eugeniord <58301986+eugeniord@users.noreply.github.com>
2023-08-05 13:56:37 +0100

    Time: 26 ms (97.68%) | Memory: 12.3 MB (65.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
+    void dfs(vector<vector<char>>& grid, int row, int col){
         int nlin = grid.size();
-
-        if (!nlin) return 0;
         int ncol = grid[0].size();
 
+        grid[row][col] = '0';
+
+        if (row - 1 >= 0 && grid[row-1][col] == '1') dfs(grid, row - 1, col);
+        if (row + 1 < nlin && grid[row+1][col] == '1') dfs(grid, row + 1, col);
+        if (col - 1 >= 0 && grid[row][col-1] == '1') dfs(grid, row, col - 1);
+        if (col + 1 < ncol && grid[row][col+1] == '1') dfs(grid, row, col + 1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int nlin = grid.size();
+        if (!nlin) return 0;
+        int ncol = grid[0].size();
         int numIslands = 0;
 
         for (int i = 0; i < nlin; ++i)
-            for (int j = 0; j < ncol; ++j){
+            for (int j = 0; j < ncol; ++j)
                 if (grid[i][j] == '1'){
                     ++numIslands;
-                    grid[i][j] = '0';
-                    queue<pair<int, int>> neighb;
-                    neighb.push({i, j});
-
-                    while (!neighb.empty()){
-                        auto [row, col] = neighb.front();
-                        neighb.pop();
-
-                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
-                            neighb.push({row-1, col}); grid[row-1][col] = '0';
-                        }
-                        if (row + 1 < nlin && grid[row+1][col] == '1') {
-                            neighb.push({row+1, col}); grid[row+1][col] = '0';
-                        }
-                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
-                            neighb.push({row, col-1}); grid[row][col-1] = '0';
-                        }
-                        if (col + 1 < ncol && grid[row][col+1] == '1') {
-                            neighb.push({row, col+1}); grid[row][col+1] = '0';
-                        }
-                    }
+                    dfs(grid, i, j);
                 }
-            }
         return numIslands;
+
     }
 };
