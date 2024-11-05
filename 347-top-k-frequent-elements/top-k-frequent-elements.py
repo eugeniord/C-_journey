4b996a3d6806ca91599576f93f116a85c03f7ba9
eugeniord <58301986+eugeniord@users.noreply.github.com>
2024-11-05 15:49:56 +0000

    Time: 5 ms (85.40%) | Memory: 13.4 MB (97.30%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,10 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        freq = {}
-        for number in nums:
-            if number in freq:
-                freq[number] += 1
-            else:
-                freq[number] = 1
-    
-        sortedDict = sorted(freq.items(), key = lambda item: item[1], reverse = True)
-        
-        firstKElements = [item[0] for item in sortedDict[:k]]
-        
-        return firstKElements
+        freq = defaultdict(int)
+        for num in nums:
+            freq[num]+=1
+        freq = sorted(freq.items(),key = lambda x:x[1], reverse = True)
+        firstK = [pair[0] for pair in freq[:k]]
+
+        return firstK
+
