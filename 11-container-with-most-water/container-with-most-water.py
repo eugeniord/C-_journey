90db7b5c609c1f7c0f612989c3e63a2b86e933a4
eugeniord <58301986+eugeniord@users.noreply.github.com>
2024-11-13 19:07:53 +0000

    Time: 75 ms (86.18%) | Memory: 19.9 MB (88.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -8,8 +8,8 @@ class Solution(object):
         p2 = len(height)-1
         maxProd = 0
         while p1 < p2:
-            prod = (p2 - p1) * min(height[p1], height[p2])
-            if prod > maxProd: maxProd = prod
-            if height[p1] >= height[p2]: p2-=1
-            else: p1+=1
+            prod = min(height[p1], height[p2]) * (p2-p1)
+            if maxProd < prod: maxProd = prod
+            if height[p1] < height[p2]: p1 += 1
+            else: p2 -= 1
         return maxProd
