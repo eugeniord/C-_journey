0197aaeeee6c9998b4abe2aee68c2cd38c3383eb
eugeniord <58301986+eugeniord@users.noreply.github.com>
2024-11-19 20:48:46 +0000

    Time: 0 ms (100.00%) | Memory: 19.5 MB (88.33%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findMin(self, node):
+        """
+        Find the node with the smallest value in the BST.
+        """
+        while node.left:
+            node = node.left
+        return node
+    def deleteNode(self, root, key):
+        """
+        :type root: Optional[TreeNode]
+        :type key: int
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return None
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left and not root.right:
+                return None
+            elif not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                succ = self.findMin(root.right)
+                root.val = succ.val
+                root.right = self.deleteNode(root.right, succ.val)
+        return root
+            
+        
